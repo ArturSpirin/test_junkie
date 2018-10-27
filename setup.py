a19236bf99b72ad7bca0c2c1d9795d324170cadc
Artur Spirin <aspirin@brightedge.com>
2018-10-27 09:49:23 -0700

    Updates to metadata & configuration for codecov

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="test_junkie",
-    version="0.2a6",
+    version="0.2a7",
     author="Artur Spirin",
     author_email="as.no.replies@gmail.com",
     description="Advanced execution framework for test scenarios",
@@ -14,10 +14,15 @@ setuptools.setup(
     url="https://github.com/ArturSpirin/test_junkie",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Topic :: Software Development :: Quality Assurance",
+        "Topic :: Software Development :: Testing",
         "Development Status :: 3 - Alpha"
     ],
 )
