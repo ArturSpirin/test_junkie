dc3f73f145ca2bd0d99d90bdea4f8d7d629ea04f
ArturSpirin <a.spirin@hotmail.com>
2019-02-02 19:04:33 -0800

    Fixes for Meta.update & Meta.get_meta after return for get_class_object() was changed

--- test_junkie/meta.py
+++ test_junkie/meta.py
@@ -14,7 +14,7 @@ class Meta:
         from test_junkie.builder import Builder
         suite_objects = Builder.get_execution_roster().values()
         for suite_object in suite_objects:
-            if suite.__class__ == suite_object.get_class_object():
+            if suite.__class__ == suite_object.get_class_object().__class__:
                 if suite_object._update_test_meta(parameter, suite_parameter, **kwargs) is True:
                     return
 
@@ -24,7 +24,7 @@ class Meta:
         from test_junkie.builder import Builder
         suite_objects = Builder.get_execution_roster().values()
         for suite_object in suite_objects:
-            if suite.__class__ == suite_object.get_class_object():
+            if suite.__class__ == suite_object.get_class_object().__class__:
                 m = suite_object._get_test_meta(parameter, suite_parameter)
                 if m is not None:
                     return m
