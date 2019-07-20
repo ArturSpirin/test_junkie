971e4d5d82ef335330b5015c08eab417ebc6c585
ArturSpirin <a.spirin@hotmail.com>
2019-07-19 22:07:01 -0700

    add Python 3.8

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Topic :: Software Development :: Quality Assurance",
