029901c6e1ada0e79380a56251c6cc2807f7043d
ArturSpirin <a.spirin@hotmail.com>
2020-02-04 22:08:08 -0800

    Documented & improved meta.py

--- test_junkie/meta.py
+++ test_junkie/meta.py
@@ -4,6 +4,12 @@ from test_junkie.decorators import synchronized
 
 
 def meta(**kwargs):
+    """
+    Use this function in order to define metadata for a @test().
+    aka @test(meta=meta(name="Example Name", expected="...", ...))
+    :param kwargs: key/value pairs
+    :return: Dictionary
+    """
     return kwargs
 
 
@@ -12,32 +18,28 @@ class Meta:
     @staticmethod
     @synchronized(threading.Lock())
     def update(suite, parameter=None, suite_parameter=None, **kwargs):
-
-        def __update_test_metadata(class_object):
-            for frame in inspect.stack():
-                try:
-                    getattr(class_object.get_class_object(), str(inspect.getframeinfo(frame[0]).function))
-                    test_function_name = str(inspect.getframeinfo(frame[0]).function)
-                    for test_object in class_object.get_test_objects():
-                        if test_object.get_function_name() == test_function_name:
-                            test_object.get_meta(parameter, suite_parameter).update(kwargs)
-                            return True
-                    return False
-                except:
-                    pass
-            return False
-
-        from test_junkie.builder import Builder
-        suite_objects = Builder.get_execution_roster().values()
-        for suite_object in suite_objects:
-            if suite.__class__ == suite_object.get_class_object():
-                if __update_test_metadata(suite_object) is True:
-                    return
+        """
+        Use this function inside a @test() where you want to update properties of the metadata
+        :param suite: Object, class object that the test is part of. Typically will be "self".
+        :param parameter: User defined parameter for the test.
+        :param suite_parameter: User defined suite parameter for the test.
+        :param kwargs: Dictionary aka {"actual_result": "3 new accounts created, 0 failed", "...": ..., ...}
+        :return: None
+        """
+        metadata = Meta.get_meta(suite, parameter, suite_parameter)
+        if metadata is not None:
+            metadata.update(kwargs)
 
     @staticmethod
     @synchronized(threading.Lock())
     def get_meta(suite, parameter=None, suite_parameter=None):
-
+        """
+        Use this function inside a @test() where you want to check the current values of the metadata for that test
+        :param suite: Object, class object that the test is part of. Typically will be "self".
+        :param parameter: User defined parameter for the test.
+        :param suite_parameter: User defined suite parameter for the test.
+        :return: Dictionary or None, depending if the meta data is defined
+        """
         def __get_test_metadata(class_object):
             for frame in inspect.stack():
                 try:
